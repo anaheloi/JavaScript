1ef2d8f5d53e95458e45fb306a7262ec658abfe1
anaheloi <111887183+anaheloi@users.noreply.github.com>
2022-10-11 08:13:29 -0300

    Create form.js

---
+++ form.js
@@ -0,0 +1,8 @@
+//form.js
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    //Adicionar aqui:
+    pacienteTr.appendChild(imcTd);
